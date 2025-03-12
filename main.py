de8e42d5cfa420bbade5acd99fdcc9ad4e95de34
Skarr222 <oskargomza54@gmail.com>
2025-03-12 15:09:13 +0100

    add functionality for player to move forward and backwards

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         player.update(dt)
-        dt = clock.tick(60) / 1000  # Now this works correctly
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
