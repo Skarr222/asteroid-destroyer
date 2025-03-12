11fd1d8057b3f867bc39cb4041640ea348dd80f3
Skarr222 <oskargomza54@gmail.com>
2025-03-12 14:18:21 +0100

    first commit, istall packages

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+
+
 def main ():
     pygame.init()
-    print("Starting Asteroids!") 
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() 
+    dt = 0 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,1))
-        
+
+        dt = clock.tick(60) / 1000  # Now this works correctly
+        screen.fill((0, 0, 1))
+        pygame.display.flip()
         
         
     
