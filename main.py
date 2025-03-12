3872f4548c1c120221c2706de67db2e048060203
Skarr222 <oskargomza54@gmail.com>
2025-03-12 15:47:19 +0100

    group the class objects

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import pygame
 from constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_KINDS,
-    ASTEROID_SPAWN_RATE,
-    ASTEROID_MAX_RADIUS,
-    PLAYER_RADIUS,
-    PLAYER_TURN_SPEED,
 )
 from player import Player
 
@@ -17,15 +11,27 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # Sprite groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 1))
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        player.update(dt)
+        for sprite in updatable:
+            sprite.update(dt)
+
         dt = clock.tick(60) / 1000
 
 
