166b79a7441c5d81e253bf4f72c825b4abc69f19
Skarr222 <oskargomza54@gmail.com>
2025-03-12 14:40:54 +0100

    create the player class and circleshape parent class to draw player figure

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_SPAWN_RATE,
+    ASTEROID_MAX_RADIUS,
+    PLAYER_RADIUS,
+)
+from player import Player
 
 
-def main ():
+def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock() 
-    dt = 0 
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         dt = clock.tick(60) / 1000  # Now this works correctly
         screen.fill((0, 0, 1))
+        player.draw(screen)
         pygame.display.flip()
-        
-        
-    
-    
+
+
 if __name__ == "__main__":
     main()
